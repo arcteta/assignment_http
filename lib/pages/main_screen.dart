3606bbe3eb6265dc9f663268e858de1e108fe5da
arcteta <ridhoadha117@gmail.com>
2024-02-04 22:29:27 +0700

    feat: add user model

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -14,7 +14,6 @@ class _MainScreenUIState extends State<MainScreenUI> {
   }
 }
 
-
 class WidgetBottomNavBar extends StatefulWidget {
   const WidgetBottomNavBar({super.key});
 
@@ -23,8 +22,64 @@ class WidgetBottomNavBar extends StatefulWidget {
 }
 
 class _WidgetBottomNavBarState extends State<WidgetBottomNavBar> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: menu 1',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: menu 2',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: menu 3',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('HTTP - Assignment'),
+        ),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled),
+            label: 'Posts',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.image_rounded),
+            label: 'Photos',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'user',
+            backgroundColor: Colors.purple,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
---
+++ lib/service/api.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:assignment_http/models/user_model.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<UserModel>> getProfileData() async {
+  try {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/users/'),
+    );
+
+    final list = json.decode(response.body) as List<dynamic>;
+    //print(list);
+    return list.map((index) => UserModel.fromJson(index)).toList();
+  } catch (error) {
+    throw Exception('Failed to load album');
+  }
+}
